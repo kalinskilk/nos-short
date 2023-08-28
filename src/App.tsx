638a1912edc9146ffa2d95378a030c1cccee6a08
Kalinski <lucascunha@xpert.com.br>
2023-08-28 10:34:51 -0300

    fix: return to browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /*  */
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import Layout from "./layout/Layout";
@@ -9,13 +9,15 @@ import ListLinksPage from "./pages/ListLinks/ListLinksPage";
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="" element={<HomePage />} />
-          <Route path="home" element={<HomePage />} />
-          <Route path="list-links" element={<ListLinksPage />} />
-        </Route>
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="" element={<HomePage />} />
+            <Route path="home" element={<HomePage />} />
+            <Route path="list-links" element={<ListLinksPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -9,9 +8,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
+
     <ToastContainer />
   </React.StrictMode>
 );
