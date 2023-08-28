81e8a7fe1f960f42fc43f995de4797a35ed72dd5
Kalinski <lucascunha@xpert.com.br>
2023-08-28 10:43:58 -0300

    fix: _redirects to publc

--- node-externals.ts
+++ node-externals.ts
@@ -1,10 +1,5 @@
-import { Plugin } from "vite";
-
-// following 2 lines are because the types from `rollup-plugin-node-externals` seems not working
-/* const require = createRequire(import.meta.url); */
-
 import { externals } from "rollup-plugin-node-externals";
-/* const { externals } = require("rollup-plugin-node-externals"); */
+import { Plugin } from "vite";
 
 export function nodeExternals(): Plugin {
   return {
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-import { nodeExternals } from "./node-externals";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodeExternals()],
+  plugins: [react()],
 });
